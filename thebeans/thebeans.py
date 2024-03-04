4ae5e5cf819ce16d26d8fc79d72ba07074dd4ce7
phillil8 <lphill57@vols.utk.edu>
2024-03-04 14:31:25 -0500

    Begin Structure

--- thebeans/thebeans.py
+++ thebeans/thebeans.py
@@ -1,1 +1,12 @@
 """Main module."""
+#creating a new ipyleaflet class for deploymnet
+#This doc shows you how the package was built. This is where you build code 
+#make sure the packages are installed in your environment
+
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_control(ipyleaflet.LayerControl())
